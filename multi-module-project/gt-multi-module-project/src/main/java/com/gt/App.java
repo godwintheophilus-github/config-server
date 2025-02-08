996f7f8dae19e8929df86c8f832b1497ffc4a9fa
gdwntheophilus <85812329+gdwntheophilus@users.noreply.github.com>
2025-02-08 12:58:57 +0530

    dsa

--- multi-module-project/gt-multi-module-project/src/main/java/com/gt/App.java
+++ multi-module-project/gt-multi-module-project/src/main/java/com/gt/App.java
@@ -1,13 +1,12 @@
 package com.gt;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        System.out.println( "Hello World!"
+        );
     }
 }
